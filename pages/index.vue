48cab3380532733130cab87780acfd10cd1b590e
Oathkitchen <amateurzhao@foxmail.com>
2019-06-29 17:50:06 +0800

    axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,42 @@
 <template>
-  <div><h1>Events</h1></div>
+  <div>
+    <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
 </template>
-
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
   head() {
     return {
       title: 'Event Listing'
     }
+  },
+  components: {
+    EventCard
+  },
+  asyncData({ $axios, error }) {
+    return (
+      $axios
+        .get('http://localhost:3000/events')
+        .then((response) => {
+          return {
+            events: response.data
+          }
+        })
+        // eslint-disable-next-line no-unused-vars
+        .catch((e) => {
+          error({
+            statusCode: 503,
+            message: 'Unable to fetch events at this time. Please try again.'
+          })
+        })
+    )
   }
 }
 </script>
-
-<style lang="scss" scoped></style>
